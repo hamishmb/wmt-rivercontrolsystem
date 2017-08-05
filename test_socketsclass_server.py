78ea89bc4c822a9f14118fb90fe61f47e30b79f4
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2017-08-05 12:42:36 +0100

    Make a server test (untested)

---
+++ test_socketsclass_server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Sockets Class (server) test for the River System Control and Monitoring Software Version 1.0
+# This file is part of the River System Control and Monitoring Software.
+# Copyright (C) 2017 Wimborne Model Town
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 or,
+# at your option, any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+import datetime
+import sys
+import os
+
+def RunStandalone():
+    #Allows the progam to run standalone as well as being a module.
+    #Do required imports.
+    import Tools
+
+    from Tools import coretools as CoreTools
+
+    print("Testing. Please stand by...")
+
+    #Create the sockets object.
+    Socket = CoreTools.Sockets("Socket")
+
+    #Set the object up.
+    Socket.SetPortNumber(30000)
+    Socket.SetServerAddress("localhost")
+
+    Socket.StartHandler()
+
+    while not Socket.IsReady(): time.sleep(0.5)
+
+    try:
+        while True:
+            while Socket.HasPendingData():
+                print(Socket.Read())
+                Socket.Pop()
+
+            time.sleep(1)
+
+    except KeyboardInterrupt:
+        #Clean up.
+        Socket.RequestHandlerExit()
+        Socket.WaitForHandlerToExit()
+        Socket.Reset()
+
+if __name__ == "__main__":
+    RunStandalone() 
