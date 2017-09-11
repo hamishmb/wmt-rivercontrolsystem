bb4e5920e5f1743c708564b3551c8c1c721d8283
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2017-09-11 14:51:46 +0100

    Main.py: Obey python style guide

--- main.py
+++ main.py
@@ -23,35 +23,35 @@
 #      It will communicate with the other pi over a socket,
 #      and the other pi will be running float_switch_monitor_standalone.py.
 
-# TODO Implement setting reading interval across monitors running on different systems.
-
-import RPi.GPIO as GPIO
 import time
 import sys
 import getopt #Proper option handler.
 import logging
 
+import RPi.GPIO as GPIO
+
 #Define global variables.
-Version = "0.9.1"
-ReleaseDate = "5/9/2017" #TODO Update when you make changes.
+VERSION = "0.9.1"
+RELEASEDATE = "11/9/2017"
 
 def usage():
     print("\nUsage: main.py [OPTION]\n\n")
     print("Options:\n")
     print("       -h, --help:               Show this help message")
-    print("       -f, --file:               Specify file to write the recordings to. Default: interactive.")
+    print("       -f, --file:               Specify file to write the recordings to.")
+    print("                                 If not specified: interactive.")
     print("main.py is released under the GNU GPL Version 3")
     print("Copyright (C) Wimborne Model Town 2017")
 
-def HandleCmdlineOptions():
+def handle_cmdline_options():
     """
     Handles commandline options.
     Usage:
 
-        tuple HandleCmdlineOptions()
+        tuple handle_cmdline_options()
     """
 
-    FileName = "Unknown"
+    file_name = "Unknown"
 
     #Check all cmdline options are valid.
     try:
@@ -65,62 +65,60 @@ def HandleCmdlineOptions():
         sys.exit(2)
 
     #Do setup. o=option, a=argument.
-    NumberOfReadingsToTake = 0 #Take readings indefinitely by default.
-
     for o, a in opts:
         if o in ["-f", "--file"]:
-            FileName = a
+            file_name = a
 
         elif o in ["-h", "--help"]:
             usage()
             sys.exit()
-    
+
         else:
             assert False, "unhandled option"
 
-    return FileName
+    return file_name
 
-def RunStandalone():
+def run_standalone():
     #Allows the progam to run standalone as well as being a module.
     #Do required imports.
     import Tools
 
-    from Tools import sensorobjects as SensorObjects
-    from Tools import monitortools as MonitorTools
-    from Tools import coretools as CoreTools
-    from Tools import sockettools as SocketTools
+    from Tools import sensorobjects as sensor_objects
+    from Tools import monitortools as monitor_tools
+    from Tools import coretools as core_tools
+    from Tools import sockettools as socket_tools
 
     Tools.coretools.logger = logger
     Tools.sockettools.logger = logger
 
     #Handle cmdline options.
-    FileName = HandleCmdlineOptions()
+    file_name = handle_cmdline_options()
 
     #Provide a connection for clients to connect to.
     logger.debug("Creating a socket for clients to connect to, please wait...")
-    Socket = SocketTools.Sockets("Socket")
-    Socket.SetPortNumber(30000)
-    Socket.StartHandler()
+    socket = socket_tools.Sockets("socket")
+    socket.set_portnumber(30000)
+    socket.start_handler()
 
     logger.debug("Done!")
 
     #Greet and get filename.
-    FileName, RecordingsFile = CoreTools.greet_and_get_filename("River System Control and Monitoring Software", FileName)
+    file_name, file_handle = core_tools.greet_and_get_filename("River System Control and Monitoring Software", file_name)
 
     #Create the devices.
-    SumpProbe = SensorObjects.ResistanceProbe("Sump Level")
-    AuxMotor = SensorObjects.Motor("Aux Motor") #SSR.
+    sump_probe = sensor_objects.ResistanceProbe("Sump Level")
+    butts_pump = sensor_objects.Motor("Aux Motor") #SSR.
 
     #Set the devices up.
-    SumpProbe.SetActiveState(False)     #Active low.
-    SumpProbe.SetPins((15, 17, 27, 22, 23, 24, 10, 9, 25, 11))
+    sump_probe.set_active_state(False)     #Active low.
+    sump_probe.set_pins((15, 17, 27, 22, 23, 24, 10, 9, 25, 11))
 
-    #Aux motor doesn't support PWM.
-    AuxMotor.SetPins(5, Input=False) #This is an output.
-    AuxMotor.SetPWMAvailable(False, -1)
+    #Butts pump doesn't support PWM.
+    butts_pump.set_pins(5, _input=False) #This is an output.
+    butts_pump.set_pwm_available(False, -1)
 
     #Reading interval.
-    ReadingInterval = 300
+    reading_interval = 300
 
     logger.info("The client(s) can connect whenever they're ready.")
     print("The client(s) can connect whenever they're ready.")
@@ -129,57 +127,58 @@ def RunStandalone():
     print("Starting to take readings. Please stand by...")
 
     #Start the monitor thread. Take readings indefinitely.
-    SumpProbeMonitorThread = MonitorTools.Monitor("Resistance Probe", SumpProbe, 0, ReadingInterval=ReadingInterval)
+    monitor = monitor_tools.Monitor("Resistance Probe", sump_probe, 0, reading_interval)
 
     #Wait until the first reading has come in so we are synchronised.
-    while not SumpProbeMonitorThread.HasData():
+    while not monitor.has_data():
         time.sleep(0.5)
 
     #Sleep a few more seconds to make sure the client is ready.
     time.sleep(10)
 
     #Setup. Prevent errors.
-    FloatSwitchReading = "Time: None State: True"
-    SumpProbeReading = "Time: Empty Time Level: -1mm Pin states: 1111111111"
+    butts_reading = "Time: None State: True"
+    sump_reading = "Time: Empty Time Level: -1mm Pin states: 1111111111"
 
     #Keep tabs on its progress so we can write new readings to the file.
     try:
         while True:
             #Exit if the resistance probe monitor thread crashes for some reason.
-            if not SumpProbeMonitorThread.IsRunning():
-                break;
+            if not monitor.is_running():
+                break
 
             #Check for new readings from the resistance probe.
-            while SumpProbeMonitorThread.HasData():
-                SumpProbeReading = SumpProbeMonitorThread.GetReading()
+            while monitor.has_data():
+                sump_reading = monitor.get_reading()
 
                 #Write any new readings to the file and to stdout.
-                logger.debug("Resistance Probe: "+SumpProbeReading)
-                print("Resistance Probe: "+SumpProbeReading)
-                RecordingsFile.write("Resistance Probe: "+SumpProbeReading+"\n")
+                logger.debug("Resistance Probe: "+sump_reading)
+                print("Resistance Probe: "+sump_reading)
+                file_handle.write("Resistance Probe: "+sump_reading+"\n")
 
             #Check for new readings from the float switch.
-            while Socket.HasPendingData():
-                FloatSwitchReading = Socket.Read()
-                Socket.Pop()
+            while socket.has_data():
+                butts_reading = socket.read()
+                socket.pop()
 
-                if FloatSwitchReading == "":
-                    #Client not ready, ignore this reading, but prevent errors. Assume the butts are full.
+                if butts_reading == "":
+                    #Client not ready, ignore this reading, but prevent errors.
+                    #Assume the butts are full.
                     logger.info("Client not ready for reading butts level. Assuming butts are full for now.")
                     print("Client not ready for reading butts level. Assuming butts are full for now.")
-                    FloatSwitchReading = "Time: None State: True"
+                    butts_reading = "Time: None State: True"
 
                 else:
                     #Write any new readings to the file and to stdout.
-                    logger.debug("Float Switch: "+FloatSwitchReading)
-                    print("Float Switch: "+FloatSwitchReading)
-                    RecordingsFile.write("Float Switch: "+FloatSwitchReading+"\n")
+                    logger.debug("Float Switch: "+butts_reading)
+                    print("Float Switch: "+butts_reading)
+                    file_handle.write("Float Switch: "+butts_reading+"\n")
 
             #Logic.
-            CoreTools.do_control_logic(SumpProbeReading, FloatSwitchReading, AuxMotor, SumpProbeMonitorThread, Socket)
+            core_tools.do_control_logic(sump_reading, butts_reading, butts_pump, monitor, socket)
 
             #Wait until it's time to check for another reading.
-            time.sleep(ReadingInterval)
+            time.sleep(reading_interval)
 
     except KeyboardInterrupt:
         #Ask the thread to exit.
@@ -187,24 +186,24 @@ def RunStandalone():
         print("Caught keyboard interrupt. Asking monitor thread to exit.")
         print("This may take a little while, so please be patient...")
 
-        SumpProbeMonitorThread.RequestExit(wait=True)
+        monitor.request_exit(wait=True)
 
     #Always clean up properly.
     logger.info("Cleaning up...")
     print("Cleaning up...")
 
-    RecordingsFile.close()
+    file_handle.close()
 
-    Socket.RequestHandlerExit()
-    Socket.WaitForHandlerToExit()
-    Socket.Reset()
+    socket.request_handler_exit()
+    socket.wait_for_handler_to_exit()
+    socket.reset()
 
     #Reset GPIO pins.
     GPIO.cleanup()
 
 if __name__ == "__main__":
-    logger = logging.getLogger('River System Control Software '+Version)
+    logger = logging.getLogger('River System Control Software '+VERSION)
     logging.basicConfig(filename='./rivercontrolsystem.log', format='%(asctime)s - %(name)s - %(levelname)s: %(message)s', datefmt='%d/%m/%Y %I:%M:%S %p')
     logger.setLevel(logging.DEBUG)
 
-    RunStandalone()
+    run_standalone()
