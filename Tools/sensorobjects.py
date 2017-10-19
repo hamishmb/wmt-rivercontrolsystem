6a6d46333bc574c2fba405aea14fb44c728cdd5e
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2017-10-19 10:57:06 +0100

    Fix missing import

--- Tools/sensorobjects.py
+++ Tools/sensorobjects.py
@@ -14,6 +14,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+VERSION = "0.9.1"
+
 #Standard Imports.
 import time
 import logging
