c51b3b2afff137714dae37fdfb7e07bce73f9d6f
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2017-11-01 14:24:22 +0000

    Write a __init__.py file for the project root to allow us to import stuff from the docs subfolder. Also fix import errors when generating documentation on non-rpi systems

---
+++ __init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# River System Control and Monitoring Software Version 0.9.1
+# This file is part of the River System Control and Monitoring Software.
+# Copyright (C) 2017 Wimborne Model Town
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License version 3 or,
+# at your option, any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from __future__ import absolute_import
+
+from . import main
+from . import Tools
