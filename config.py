f11eb1c876fcf3d5de5a5ae379001a7763a724e0
Hamish McIntyre-Bhatty <hamishmb@live.co.uk>
2018-12-30 19:41:14 +0000

    Fix syntax errors and duplicated keys in config.py

--- config.py
+++ config.py
@@ -142,7 +142,8 @@ SITE_SETTINGS = {
                             "ID":           "SOCK4",
                             "Name":         "Buttspi Socket",
                             "PortNumber":   30004
-                        }
+                        },
+
                     #For connection to Wendy Street Butts Pi Gate Valve.
                     "Buttspi Gate Valve Socket V4":
                         {
@@ -150,7 +151,7 @@ SITE_SETTINGS = {
                             "Name":         "Buttspi Gate Valve V4 Socket",
                             "PortNumber":   30014
                         }
-                 },
+                },
 
             #Local probes.
             "Probes":
@@ -175,14 +176,11 @@ SITE_SETTINGS = {
                     {
                         "Type":  "Motor",
                         "ID":    "SUMP:P0",
-        "ServerAddress": "192.168.0.2",
-        "ServerPort": 30014
+                        "ServerAddress": "192.168.0.2",
+                        "ServerPort": 30014,
 
                         "Name":  "Sump to Butts Pump",
-                        "Class": Tools.deviceobjects
-        "ServerAddress": "192.168.0.2",
-        "ServerPort": 30014
-.Motor,
+                        "Class": Tools.deviceobjects,
                         "Pins":  (5)
                     },
 
@@ -226,31 +224,29 @@ SITE_SETTINGS = {
                         "Default Interval": 30
                     }
                 },
-            
-        },
 
             "ServerAddress": "192.168.0.2",
             "ServerPort": 30004
-        }
+        },
 
     #Gate Valves.
     "V4":
-    {
-        "Type": "Gate Valve",
-        "ID":   "V4",
-        "Name": "Butts Farm Gate Valve",
-        "Class": Tools.deviceobjects.GateValve,
+        {
+            "Type": "Gate Valve",
+            "ID":   "V4",
+            "Name": "Butts Farm Gate Valve",
+            "Class": Tools.deviceobjects.GateValve,
 
-        "Pins":  (17, 27, 19),
-        "posTolerance": 1,
-        "maxOpen": 99,
-        "minOpen": 1,
-        "refVoltage": 3.3,
-        "Default Interval": 10
+            "Pins":  (17, 27, 19),
+            "posTolerance": 1,
+            "maxOpen": 99,
+            "minOpen": 1,
+            "refVoltage": 3.3,
+            "Default Interval": 10,
 
-        #Config for server connection.
-        "ServerAddress": "192.168.0.2",
-        "ServerPort": 30014
-    }
+            #Config for server connection.
+            "ServerAddress": "192.168.0.2",
+            "ServerPort": 30014
+        }
 
 }
